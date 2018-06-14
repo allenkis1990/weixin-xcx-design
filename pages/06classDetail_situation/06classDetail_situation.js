3511405c81904f4f2dec693e31289e3ec0cda9bb
Allen <allenkis1990@163.com>
2018-06-14 17:41:18 +0800

    Initial commit

---
+++ pages/06classDetail_situation/06classDetail_situation.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    percentage: 50
+  },
+  onReady: function (e) {
+    // // 使用 wx.createContext 获取绘图上下文 context
+    var firstCanvas = wx.createCanvasContext('firstCanvas')
+    var secondCanvas = wx.createCanvasContext('secondCanvas')
+    firstCanvas.setStrokeStyle('#50c6b8');
+    firstCanvas.setLineWidth(1);
+    firstCanvas.arc(51, 51, 50, 0, 2 * Math.PI, false);
+    firstCanvas.stroke();
+    firstCanvas.draw();
+    secondCanvas.setStrokeStyle("#ffffff")
+    secondCanvas.setLineWidth(4)
+    secondCanvas.arc(51, 51, 44, -0.5 * Math.PI, (0.01 * this.data.percentage - 0.25) * 2 * Math.PI, false)
+    secondCanvas.stroke()
+    secondCanvas.draw()
+  }
+})
